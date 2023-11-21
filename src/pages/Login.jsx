22f8ab8e8bca6bf23665aa6fba2c88479d092ff1
nugraha270203 <nugrahamuhammadtaufik2003@gmail.com>
2023-11-22 02:39:42 +0700

    k

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,19 +3,32 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../component/navbar";
 import phone from "../assets/phone.png";
 import Bodi from "../component/Bodi";
+import footer from "../assets/footer.png"
 function Login() {
-    return (
-        <>
-            <Navbar />
-            <div className="container">
-                <div class="text-danger text-lg-center">Login</div>
-                <div class="text-lg-center"><p> Welcome Back! Please Log in to access your account.</p></div>
-                <div class="text-lg-center">
-                    <img src={phone} alt="" />
-                </div>
-            </div>
-           <Bodi />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="container mt-5">
+        <div class="text-danger text-lg-center">Login</div>
+        <div class="text-lg-center">
+          <p> Welcome Back! Please Log in to access your account.</p>
+        </div>
+        <div class="text-lg-center">
+          <img src={phone} alt="" />
+        </div>
+      </div>
+      <Bodi />
+
+      <div id="footer" className="container-fluid mt-5" style={{ padding: 0 }}>
+        <div className="landing-section">
+          <img
+            src={footer}
+            alt=""
+            style={{ width: "100%", height: "auto", position: "relative" }}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 export default Login;
--- src/pages/Regis.jsx
+++ src/pages/Regis.jsx
@@ -6,7 +6,6 @@ function Regis() {
   return (
     <>
       <Navbar />
-      
 
       <div className="container-fluid" style={{ padding: 0 }}>
         <div className="landing-section">
@@ -30,54 +29,54 @@ function Regis() {
                 />
               </div>
             </div>
-            <div className="col-md-5 offset-md-1 mt-5">
-            <form>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              id="namaf"
-              placeholder="Enter First Name"
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              id="namal"
-              placeholder="Enter Last Name"
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              id="email"
-              placeholder="Enter Email"
-            />
-          </div>
-          <div className="form-group md-8">
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Password"
-            />
-          </div>
-          <div className="form-group text-center">
-            <button
-              type="button"
-              className="btn btn-danger btn-lg btn-sm rounded-pill"
-              style={{ width: "63%" }}
-            >
-              Register
-            </button>
-          </div>
-          <div class="form-group text-center">
-            Already have an account?
-            <a href="/login">Sign in</a>
-          </div>
-        </form>
+            <div className="col-md-4 offset-md-1 mt-5">
+              <form>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control "
+                    id="namaf"
+                    placeholder="Enter First Name"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="namal"
+                    placeholder="Enter Last Name"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="email"
+                    placeholder="Enter Email"
+                  />
+                </div>
+                <div className="form-group md-8">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="form-group text-center">
+                  <button
+                    type="button"
+                    className="btn btn-danger btn-lg btn-sm rounded-pill"
+                    style={{ width: "63%" }}
+                  >
+                    Register
+                  </button>
+                </div>
+                <div class="form-group text-center">
+                  Already have an account?
+                  <a href="/login">Sign in</a>
+                </div>
+              </form>
             </div>
           </div>
         </div>
