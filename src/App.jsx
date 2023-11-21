ea2d604937fbc706b8e4dca41a5b60f3b7bf1182
nugraha270203 <nugrahamuhammadtaufik2003@gmail.com>
2023-11-21 14:18:19 +0700

    komen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+
+// JANAGAN LUPA IMPORT HALAMAN YANG SUDAH DIBUAT DI FOLDER PAGES
 import Home from "./pages/home";
 import Login from "./pages/login";
+
+
 import {
   createBrowserRouter,
   RouterProvider,
@@ -8,6 +12,7 @@ import {
   Link,
 } from "react-router-dom";
 
+// PANGGIL FUNCTION UNTUK ROUTER
 const router = createBrowserRouter([
   {path:"/", element:<Home />},
   {path:"/Login", element:<Login />}
