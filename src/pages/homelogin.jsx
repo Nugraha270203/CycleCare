d09f36145de729bd874c45abb2b76cb7c5e15b90
nugraha270203 <nugrahamuhammadtaufik2003@gmail.com>
2023-11-21 20:58:21 +0700

    udah navbar

---
+++ src/pages/homelogin.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "../component/navbar";
+function Home() {
+    return (
+      <>
+        <Navbar />
+        <div className="container">
+        
+        </div>
+
+      </>
+    );
+  }
+
+export default Home;
