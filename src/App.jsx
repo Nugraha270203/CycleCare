8cc728149408aea66ff9e248411f42f7385cfac7
nugraha270203 <nugrahamuhammadtaufik2003@gmail.com>
2023-11-22 22:54:22 +0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,12 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 // JANAGAN LUPA IMPORT HALAMAN YANG SUDAH DIBUAT DI FOLDER PAGES
 import Home from "./pages/home";
 import Login from "./pages/login";
-<<<<<<< HEAD
 import Profile from "./pages/Profile";
-=======
 import Regis from "./pages/Regis";
 import More from "./pages/more";
 import Otp from "./pages/Otp";
 import Article from "./pages/Article";
 import Homelogin from "./pages/Homelogin";
->>>>>>> 84f092961c41611b9c515b82622f5791ae97524b
 
 import {
   createBrowserRouter,
@@ -27,16 +24,13 @@ import {
 const router = createBrowserRouter([
   {path:"/", element:<Home />},
   {path:"/Login", element:<Login />},
-<<<<<<< HEAD
-  {path:"/Profile", element:<Profile />}
-=======
+  {path:"/Profile", element:<Profile />},
   {path:"/Regis", element:<Regis />},
   {path:"/More", element:<More />},
   {path:"/Otp", element:<Otp />},
   {path:"/Article", element:<Article />},
   {path:"/Homelogin", element:<Homelogin />}
 
->>>>>>> 84f092961c41611b9c515b82622f5791ae97524b
 ]);
 
 
