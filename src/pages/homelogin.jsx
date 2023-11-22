c05f0c6b49257df7ff22ffa2e2cb803246c398e9
nugraha270203 <nugrahamuhammadtaufik2003@gmail.com>
2023-11-22 21:29:47 +0700

    pus

--- src/pages/homelogin.jsx
+++ src/pages/homelogin.jsx
@@ -4,6 +4,11 @@ import Navbarlogin from "../component/Navbarlogin";
 import Bghomelogin from "../assets/bghomelogin.png";
 import { Link } from "react-router-dom";
 import foto from "../assets/foto.png"
+import card from "../assets/card.png"
+import card2 from "../assets/card2.png"
+import card3 from "../assets/card3.png"
+import card4 from "../assets/card4.png"
+import card5 from "../assets/card5.png"
 
 function Homelogin() {
   return (
@@ -18,84 +23,122 @@ function Homelogin() {
           />
         </div>
       </div>
+
       <div className="container">
         <div className="container-fluid">
           <div className="text-center mx-auto" style={{ width: "250px" }}>
 
             <div className="d-flex justify-content-center">
               <div className="d-flex">
-                <Link to="/Article" style={{ textDecoration: "none" }}>
-                  <div className="card shadow-lg mt-5 mb-1 rounded-4 mx-4" style={{ width:"11rem", height:"15rem" }}>
+                <Link className="mx-3" to="/Article" style={{ textDecoration: "none" }}>
+                  <div className="card shadow-lg mt-5 mb-1 rounded-4 " style={{ width: "12rem", height: "15rem" }}>
                     <img
-                      src={foto}
+                      src={card}
                       className="card-img-top img-fluid rounded-top-4 "
                       alt="..."
                     />
-                    <div className="card-body">
-                      <p className="card-text text-start fw-bold">
-                        Some quick example 
-                      </p>
-                    </div>
+
                   </div>
                 </Link>
 
-                <Link to="/Article" style={{ textDecoration: "none" }}>
-                  <div className="card shadow-lg mt-5 mb-1 rounded-4 mx-4" style={{ width:"11rem", height:"15rem" }}>
+                <Link className="mx-3" to="/Article" style={{ textDecoration: "none" }}>
+                  <div className="card shadow-lg mt-5 mb-1 rounded-4 " style={{ width: "12rem", height: "15rem" }}>
                     <img
-                      src={foto}
+                      src={card2}
                       className="card-img-top img-fluid rounded-top-4 "
                       alt="..."
                     />
-                    <div className="card-body">
-                      <p className="card-text text-start fw-bold">
-                        Some quick example 
-                      </p>
-                    </div>
+
                   </div>
                 </Link>
-                <Link to="/Article" style={{ textDecoration: "none" }}>
-                  <div className="card shadow-lg mt-5 mb-1 rounded-4 mx-4" style={{ width:"11rem", height:"15rem" }}>
+                <Link className="mx-3" to="/Article" style={{ textDecoration: "none" }}>
+                  <div className="card shadow-lg mt-5 mb-1 rounded-4 " style={{ width: "12rem", height: "15rem" }}>
                     <img
-                      src={foto}
+                      src={card3}
                       className="card-img-top img-fluid rounded-top-4 "
                       alt="..."
                     />
-                    <div className="card-body">
-                      <p className="card-text text-start fw-bold">
-                        Some quick example 
-                      </p>
-                    </div>
+
                   </div>
                 </Link>
 
-                <Link to="/Article" style={{ textDecoration: "none" }}>
-                  <div className="card shadow-lg mt-5 mb-1 rounded-4 mx-4" style={{ width:"11rem", height:"15rem" }}>
+                <Link className="mx-3" to="/Article" style={{ textDecoration: "none" }}>
+                  <div className="card shadow-lg mt-5 mb-1 rounded-4 " style={{ width: "12rem", height: "15rem" }}>
                     <img
-                      src={foto}
+                      src={card4}
                       className="card-img-top img-fluid rounded-top-4 "
                       alt="..."
                     />
-                    <div className="card-body">
-                      <p className="card-text text-start fw-bold">
-                        Some quick example 
-                      </p>
-                    </div>
+
                   </div>
                 </Link>
-                <Link to="/Article" style={{ textDecoration: "none" }}>
-                  <div className="card shadow-lg mt-5 mb-1 rounded-4 mx-4" style={{ width:"11rem", height:"15rem" }}>
+                <Link className="mx-3" to="/Article" style={{ textDecoration: "none" }}>
+                  <div className="card shadow-lg mt-5 mb-1 rounded-4 " style={{ width: "12rem", height: "15rem" }}>
                     <img
-                      src={foto}
+                      src={card5}
                       className="card-img-top img-fluid rounded-top-4 "
                       alt="..."
                     />
-                    <div className="card-body">
-                      <p className="card-text text-start fw-bold">
-                        Some quick example 
-                      </p>
-                    </div>
+
                   </div>
-                </Link>               
+                </Link>
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div className="container">
+        <div className="container-fluid">
+          <div className="text-center mx-auto" style={{ width: "250px" }}>
+
+            <div className="d-flex justify-content-center">
+              <div className="d-flex">
+                
+
+                <Link className="mx-3" to="/Article" style={{ textDecoration: "none" }}>
+                  <div className="card shadow-lg mt-5 mb-1 rounded-4 " style={{ width: "12rem", height: "15rem" }}>
+                    <img
+                      src={card2}
+                      className="card-img-top img-fluid rounded-top-4 "
+                      alt="..."
+                    />
+
+                  </div>
+                </Link>
+                <Link className="mx-3" to="/Article" style={{ textDecoration: "none" }}>
+                  <div className="card shadow-lg mt-5 mb-1 rounded-4 " style={{ width: "12rem", height: "15rem" }}>
+                    <img
+                      src={card3}
+                      className="card-img-top img-fluid rounded-top-4 "
+                      alt="..."
+                    />
+
+                  </div>
+                </Link>
+
+                <Link className="mx-3" to="/Article" style={{ textDecoration: "none" }}>
+                  <div className="card shadow-lg mt-5 mb-1 rounded-4 " style={{ width: "12rem", height: "15rem" }}>
+                    <img
+                      src={card4}
+                      className="card-img-top img-fluid rounded-top-4 "
+                      alt="..."
+                    />
+
+                  </div>
+                </Link>
+                <Link className="mx-3" to="/Article" style={{ textDecoration: "none" }}>
+                  <div className="card shadow-lg mt-5 mb-1 rounded-4 " style={{ width: "12rem", height: "15rem" }}>
+                    <img
+                      src={card5}
+                      className="card-img-top img-fluid rounded-top-4 "
+                      alt="..."
+                    />
+
+                  </div>
+                </Link>
 
 
               </div>
@@ -104,10 +147,20 @@ function Homelogin() {
         </div>
       </div>
 
-    
+      <div className="container">
+        <div className="container-fluid">
+          <div class="card mt-5 mb-5" style={{width: "100%"}}>
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+          </div>
+        </div>
+      </div>
 
-      </>
-    );
+
+
+    </>
+  );
 }
 
 export default Homelogin;
