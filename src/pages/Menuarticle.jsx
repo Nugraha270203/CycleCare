4eae063b2ed3e7602c95351c349d326ca23996fa
nugraha270203 <nugrahamuhammadtaufik2003@gmail.com>
2023-11-22 23:15:23 +0700

    sa

--- src/pages/Menuarticle.jsx
+++ src/pages/Menuarticle.jsx
@@ -6,7 +6,17 @@ function Menuarticle() {
             <Navbarlogin />
             <div className="container">
                 <div className="container-fluid">
+                    <h2 className="mt-5 fw-bold">Article</h2>
+                    <p style={{ fontSize: "25px" }}> Yuk baca artikel untuk menambah wawasan kamu!</p>
+                    <div className="d-flex justify-content-center">
+                        <div class="card" >
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                
+                            </div>
+                        </div>
 
+                    </div>
                 </div>
             </div>
         </>
