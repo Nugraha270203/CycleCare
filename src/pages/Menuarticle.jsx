5f2b85b264087c3e297274423a664d095c18fb4a
nugraha270203 <nugrahamuhammadtaufik2003@gmail.com>
2023-11-22 22:53:24 +0700

    b

---
+++ src/pages/Menuarticle.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Menuarticle() {
+    return (
+        <>
+            <div className="container">
+                <div className="container-fluid">
+
+                </div>
+            </div>
+        </>
+    )
+}
+export default Menuarticle;
