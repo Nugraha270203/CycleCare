2b285c9b0dd90630c7149038e799e6e7fc60516c
nugraha270203 <nugrahamuhammadtaufik2003@gmail.com>
2023-12-11 22:15:54 +0700

    a

--- Backend/server.jsx
+++ Backend/server.jsx
@@ -5,35 +5,12 @@ const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const path = require('path');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const crypto = require('crypto'); // Tambahkan library crypto
-
+const crypto = require('crypto'); 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const generateToken = (user) => {
-  return jwt.sign({ userId: user.id }, secretKey, { expiresIn: '1h' });
-};
-
-// Fungsi untuk membandingkan kata sandi dengan hash
-const comparePasswords = async (password, hashedPassword) => {
-  return await bcrypt.compare(password, hashedPassword);
-};
-
-
-// Fungsi untuk menghasilkan kunci rahasia
-const generateSecretKey = () => {
-  return crypto.randomBytes(32).toString('hex'); // 32 byte = 256 bit
-};
-
-// Gunakan fungsi generateSecretKey() di bawah ini
-const secretKey = generateSecretKey();
-
-// ... (lanjutan kode Anda)
-
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -44,7 +21,6 @@ const storage = multer.diskStorage({
   }
 });
 const upload = multer({ storage: storage });
-
 // Koneksi ke database MySQL
 const db = mysql.createConnection({
   host: "localhost",
@@ -52,7 +28,6 @@ const db = mysql.createConnection({
   password: "",
   database: "cyclecare"
 });
-
 db.connect((err) => {
   if (err) {
     console.error("Error connecting to database:", err);
@@ -73,10 +48,10 @@ app.get("/Admin/artikel", (req, res) => {
   });
 });
 
-// Menyajikan file statis dari direktori gambar
+// File dari direktori gambar
 app.use('/assets/gambar', express.static(path.join(__dirname, 'src', 'assets', 'gambar')));
 
-// Endpoint untuk menambah artikel dengan gambar
+// untuk menambah artikel dengan gambar
 app.post("/Admin/tambahartikel", upload.single('foto'), (req, res) => {
   const j_artikel = req.body.j_artikel;
   const foto = req.file.filename; // Nama file gambar yang diunggah
@@ -92,7 +67,7 @@ app.post("/Admin/tambahartikel", upload.single('foto'), (req, res) => {
     return res.json(data);
   });
 });
-
+// untuk menghapus artikel
 app.delete("/Admin/artikel/:id", (req, res) => {
   const id = req.params.id;
   const sql = "DELETE FROM artikel WHERE id = ?";
@@ -104,7 +79,7 @@ app.delete("/Admin/artikel/:id", (req, res) => {
     return res.json(data);
   });
 });
-
+// Endpoint untuk registrasi
 app.post('/regis', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -112,7 +87,7 @@ app.post('/regis', (req, res) => {
   const lastname = req.body.lastname;
   const username = req.body.username;
 
-  // Validasi apakah email sudah ada dalam database
+  // untuk memvalidasi apakah email sudah ada dalam database
   db.query(
     "SELECT * FROM user WHERE email = ?",
     [email],
@@ -144,11 +119,7 @@ app.post('/regis', (req, res) => {
     }
   );
 });
-
-
-
-
-
+// Endpoint untuk login
 app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -172,7 +143,7 @@ app.post('/login', (req, res) => {
 });
 
 
-// Mulai server pada port 8082
+// Mmemulai server pada port 8082
 app.listen(8082, () => {
   console.log('Server aktif...');
 });
