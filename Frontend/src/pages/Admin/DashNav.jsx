2e3977fb50dbb6174485388e6616ff062666e717
sul0211 <syamsulbuchori5@gmail.com>
2023-12-11 22:26:37 +0700

    saxz

--- Frontend/src/pages/Admin/DashNav.jsx
+++ Frontend/src/pages/Admin/DashNav.jsx
@@ -8,12 +8,19 @@ import art1 from "../../assets/art1.png";
 import art12 from "../../assets/art12.png";
 import prof from "../../assets/prof.png";
 import con1 from "../../assets/con1.png";
+import con2 from "../../assets/con2.png";
+import q2 from "../../assets/q2.png";
 
 function DashNav() {
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => {
+        setOpen(!open);
+    };
     return (
         <>
             <div id="wrapper">
-                <ul class="navbar-nav bg-white sidebar sidebar-dark accordion" id="accordionSidebar">
+                <ul class="navbar-nav bg-grey sidebar sidebar-dark accordion" id="accordionSidebar">
                     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
                         <div class="sidebar-brand-icon rotate-n-15">
 
@@ -28,18 +35,18 @@ function DashNav() {
                             <img src={q1} alt="" width="10%" height="auto" />
                             <span>  Dashboard</span></a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link collapsed text-danger" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                            aria-expanded="true" aria-controls="collapseTwo">
-                            <img src={art1} alt="" width="10%" height="auto" />
-                            <span> Articles</span>
-                        </a>
-                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                            <div class="bg-red py-2 collapse-inner rounded">
-                                <a class="collapse-item" href="#">Create Articles</a>
-                            </div>
-                        </div>
-                    </li>
+                    <div className="dropdown">
+                        <img src={con1} alt="" width="10%" height="auto"/>
+                        <span className="text-danger" onClick={handleOpen}>Article</span>
+                        {open ? (
+                            <ul className="menu">
+                                <li className="menu-item">
+                                    <button>Create Article</button>
+                                </li>   
+                            </ul>
+                        ) : null}
+                        
+                    </div>
                     <li class="nav-item">
                         <a class="nav-link collapsed text-danger" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                             aria-expanded="true" aria-controls="collapseUtilities">
@@ -53,11 +60,24 @@ function DashNav() {
                             </div>
                         </div>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link collapsed text-danger" href="/Admin/tambahartikel" data-toggle="collapse" data-target="#collapseUtilities"
+                            aria-expanded="true" aria-controls="collapseUtilities">
+                            <img src={q2} alt="" width="10%" height="auto" />
+                            <span> Community</span>
+                        </a>
+                        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                            data-parent="#accordionSidebar">
+                            <div class="bg-white py-2 collapse-inner rounded">
+                                <a class="collapse-item" href="utilities-color.html">Colors</a>
+                            </div>
+                        </div>
+                    </li>
                 </ul>
 
                 <div id="content-wrapper" class="d-flex flex-column">
                     <div id="content">
-                        <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                        <nav className="navbar navbar-expand navbar-light topbar mb-4 static-top shadow" style={{ backgroundColor: "#800000" }}>
                             <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                                 <i class="fa fa-bars"></i>
                             </button>
@@ -66,6 +86,12 @@ function DashNav() {
                                     <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
                                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         <img src={con1} />
+                                    </a>
+                                </li>
+                                <li class="nav-item dropdown no-arrow mx-1">
+                                    <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <img src={con2} />
                                     </a>
                                 </li>
                                 <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
@@ -114,78 +140,78 @@ function DashNav() {
                             </ul>
                         </nav>
                         <div className="col">
-                    <div className="container">
-                        <div className="container-fluid">
-                            <h1>DASHBOARD</h1>
-                            <div className="row">
-                            <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Total Video</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">5</div>
+                            <div className="container">
+                                <div className="container-fluid">
+                                    <h1>DASHBOARD</h1>
+                                    <div className="row">
+                                        <div class="col-xl-3 col-md-6 mb-4">
+                                            <div class="card border-left-primary shadow h-100 py-2">
+                                                <div class="card-body">
+                                                    <div class="row no-gutters align-items-center">
+                                                        <div class="col mr-2">
+                                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                                Total Video</div>
+                                                            <div class="h5 mb-0 font-weight-bold text-gray-800">5</div>
+                                                        </div>
+                                                        <div class="col-auto">
+                                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                        <div class="col-xl-3 col-md-6 mb-4">
+                                            <div class="card border-left-primary shadow h-100 py-2">
+                                                <div class="card-body">
+                                                    <div class="row no-gutters align-items-center">
+                                                        <div class="col mr-2">
+                                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                                Total Artikel</div>
+                                                            <div class="h5 mb-0 font-weight-bold text-gray-800">10</div>
+                                                        </div>
+                                                        <div class="col-auto">
+                                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-3 col-md-6 mb-4">
+                                            <div class="card border-left-primary shadow h-100 py-2">
+                                                <div class="card-body">
+                                                    <div class="row no-gutters align-items-center">
+                                                        <div class="col mr-2">
+                                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                                Total Pengguna</div>
+                                                            <div class="h5 mb-0 font-weight-bold text-gray-800">5</div>
+                                                        </div>
+                                                        <div class="col-auto">
+                                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-3 col-md-6 mb-4">
+                                            <div class="card border-left-primary shadow h-100 py-2">
+                                                <div class="card-body">
+                                                    <div class="row no-gutters align-items-center">
+                                                        <div class="col mr-2">
+                                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                                Total Postingan Komunitas</div>
+                                                            <div class="h5 mb-0 font-weight-bold text-gray-800">5</div>
+                                                        </div>
+                                                        <div class="col-auto">
+                                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Total Artikel</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">10</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Total Pengguna</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">5</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Total Postingan Komunitas</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">5</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                     </div>
                 </div>
             </div>
